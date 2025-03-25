012d77c0ac036ae84fe00c66668817c8ade52d20
overhauldev <archietownsend999@gmail.com>
2025-03-25 16:56:30 +0000

    refactor NavBar layout and styles; update CSS for container responsiveness and fix variable syntax

--- src/components/NavBar/main-nav.tsx
+++ src/components/NavBar/main-nav.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const MainNav = () => {
 	return (
-		<div className="hidden md:flex">
-			<Link to="/">
+		<div className="hidden md:flex w-full items-center">
+			<Link to="/" className="mr-auto">
 				<Recycle className="w-8 h-8" />
 			</Link>
-			<nav className="flex items-center gap-3 lg:gap-4 ml-8">
+			<nav className="flex items-center gap-3 lg:gap-4 ml-auto p-12">
 				<Link to="/about">About</Link>
 				<Link to="/contact">Contact</Link>
 			</nav>
